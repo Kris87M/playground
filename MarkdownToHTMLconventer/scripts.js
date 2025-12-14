80e2782ebb0a64198e189a1d108622ee27609c7c
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-12-14 20:46:06 +0100

    Feat: implement full markdown parsing for headings, bold, italic, links, images, and blockquotes

--- MarkdownToHTMLconventer/scripts.js
+++ MarkdownToHTMLconventer/scripts.js
@@ -7,5 +7,25 @@ markdownInput.addEventListener("input", () => {
 });
 
 function convertMarkdown() {
+  const markdownText = markdownInput.value;
+  const headingThreeRegex = /^### (.+)$/mg;
+  const headingTwoRegex = /^## (.+)$/mg;
+  const headingOneRegex = /^# (.+)$/mg;
+  const imageRegex = /!\[(.+?)\]\((.+?)\)/g;
+  const linkRegex = /\[(.+?)\]\((.+?)\)/g;
+  const boldRegex = /(\*\*|__)(.+?)\1/g;
+  const italicRegex = /(\*|_)(.+?)\1/g;
+  const quoteRegex = /^> (.+)/gm;
 
+  let htmlOutput = markdownText.replace(headingThreeRegex, "<h3>$1</h3>");
+  htmlOutput = htmlOutput.replace(headingTwoRegex, "<h2>$1</h2>");
+  htmlOutput = htmlOutput.replace(headingOneRegex, "<h1>$1</h1>");
+  htmlOutput = htmlOutput.replace(imageRegex, "<img alt='$1' src='$2'>")
+  htmlOutput = htmlOutput.replace(linkRegex, "<a href='$2'>$1</a>")
+  htmlOutput = htmlOutput.replace(boldRegex, "<strong>$2</strong>")
+  htmlOutput = htmlOutput.replace(italicRegex, "<em>$2</em>");
+  htmlOutput = htmlOutput.replace(quoteRegex, "<blockquote>$1</blockquote>")
+  rawHTMLoutput.textContent = htmlOutput;
+  preview.innerHTML = htmlOutput;
+  return htmlOutput;
 }
