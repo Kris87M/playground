ec30dd717ead09c2df1879f81114e35d59890a92
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-12-14 20:44:19 +0100

    Setup: initialize HTML elements and input listener

---
+++ MarkdownToHTMLconventer/scripts.js
@@ -0,0 +1,11 @@
+const markdownInput = document.getElementById('markdown-input');
+const rawHTMLoutput = document.getElementById('html-output');
+const HTMLpreview = document.getElementById('preview');
+
+markdownInput.addEventListener("input", () => {
+  convertMarkdown()
+});
+
+function convertMarkdown() {
+
+}
